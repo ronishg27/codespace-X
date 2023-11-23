b181d21a7db54a2f7dc4820171dfba7f1db68c24
ronishg27 <iamrg2060@gmail.com>
2023-11-23 14:42:07 +0545

    removed bcrypt from userLoginRoute and added to usermodeljs

--- server/src/routes/signup.route.js
+++ server/src/routes/signup.route.js
@@ -1,19 +1,17 @@
 import express from "express";
 import { User } from "../models/user.models.js";
-import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   try {
     const { username, fullname, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
     const savedUser = await User.create({
       username,
       fullname,
       email,
-      password: hashedPassword,
-    }); 
+      password,
+    });
     console.log("User saved successfully. ", savedUser);
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
