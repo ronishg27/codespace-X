25a023237ae2d791c600d356472b4e4ab762bf83
ronishg27 <iamrg2060@gmail.com>
2023-12-26 22:53:37 +0545

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,37 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Navbar from "./components/navbar";
-import ForumPostForm from "./components/forumPostForm";
-import ForumPost from "./components/forumPost";
-import LoginPage from "./components/loginPage";
-import SignupPage from "./components/signupPage";
+// import Navbar from "./components/navbar";
+// import ForumPostForm from "./components/forumPostForm";
+// import ForumPost from "./components/forumPost";
+// import LoginPage from "./components/loginPage";
+// import SignupPage from "./components/signupPage";
+// import Profile from "./components/profile";
+
+import {
+	Navbar,
+	ForumPost,
+	ForumPostForm,
+	LoginPage,
+	SignupPage,
+} from "./index.js";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<SignupPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/create-post" element={<ForumPostForm />} />
-          <Route path="/feed" element={<ForumPost />} />
-        </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Navbar />
+			<Router>
+				<Routes>
+					<Route path="/" element={<SignupPage />} />
+					<Route path="/signup" element={<SignupPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/create-post" element={<ForumPostForm />} />
+					<Route path="/feed" element={<ForumPost />} />
+					{/* <Route path="/profile" element={<Profile />} /> */}
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import Navbar from "./components/navbar";
+import ForumPostForm from "./components/forumPostForm";
+import ForumPost from "./components/forumPost";
+import LoginPage from "./components/loginPage";
+import SignupPage from "./components/signupPage";
+// import Profile from "./components/profile";
+
+export { Navbar, ForumPost, ForumPostForm, LoginPage, SignupPage };
