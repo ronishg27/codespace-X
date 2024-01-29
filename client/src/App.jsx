eb756df78ddbf5e7ae308cf8d92c7afaac1d187c
ronishg27 <iamrg2060@gmail.com>
2024-01-29 14:44:50 +0545

    working on redux-toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,42 @@
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import {
+	Routes,
+	Route,
+	BrowserRouter as Router,
+	// Outlet,
+} from "react-router-dom";
 
 import { Navbar, SignupPage } from "./index.js";
 import Account from "./components/account.jsx";
 import User from "./components/user.jsx";
-
 import NotFound from "./components/notFound.jsx";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./services/auth.service.js";
+import { login, logout } from "./store/authSlice.js";
 
 function App() {
+	// eslint-disable-next-line no-unused-vars
+	const [loading, setLoading] = useState(true);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		authService
+			.getUser()
+			.then((userData) => {
+				if (userData) {
+					dispatch(login({ userData }));
+				} else {
+					dispatch(logout());
+				}
+			})
+			.finally(() => setLoading(false));
+	});
+
 	return (
 		<>
 			<Navbar />
+
+			{/* <Outlet /> */}
 
 			<Router>
 				<Routes>
